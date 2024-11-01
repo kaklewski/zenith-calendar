93c113cea341d746554193ad54d1614dea9b632e
Oskar Kąklewski <105233545+kaklewski@users.noreply.github.com>
2024-11-01 16:59:27 +0100

    feat: Create event adding modal

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -14,6 +14,10 @@ import {
 } from 'date-fns'
 import { formatDate } from '../utils/formatDate'
 import { cc } from '../utils/cc'
+import { useEvents } from '../context/useEvents'
+import Modal, { ModalProps } from './Modal'
+import { UnionOmit } from '../utils/types'
+import { Event } from '../context/Events'
 
 export function Calendar() {
 	const [selectedMonth, setSelectedMonth] = useState(new Date())
@@ -27,9 +31,6 @@ export function Calendar() {
 		})
 		return eachDayOfInterval({ start: firstWeekStart, end: lastWeekEnd })
 	}, [selectedMonth])
-
-	// console.log(firstWeekStart,' - ', lastWeekEnd);
-	// console.log(calendarDays)
 
 	return (
 		<div className='calendar'>
@@ -51,7 +52,12 @@ export function Calendar() {
 						&gt;
 					</button>
 				</div>
-				<span className='month-title'>{formatDate(selectedMonth, {month: 'long', year: 'numeric'})}</span>
+				<span className='month-title'>
+					{formatDate(selectedMonth, {
+						month: 'long',
+						year: 'numeric',
+					})}
+				</span>
 			</div>
 			<div className='days'>
 				{calendarDays.map((day, index) => (
@@ -74,6 +80,9 @@ type CalendarDayProps = {
 }
 
 function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
+	const [isNewEventModalOpen, setIsNewEventModalOpen] = useState(false)
+	const { addEvent } = useEvents()
+
 	return (
 		<div
 			className={cc(
@@ -90,7 +99,11 @@ function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
 				<div className={cc('day-number', isToday(day) && 'today')}>
 					{formatDate(day, { day: 'numeric' })}
 				</div>
-				<button className='add-event-btn'>+</button>
+				<button
+					className='add-event-btn'
+					onClick={() => setIsNewEventModalOpen(true)}>
+					+
+				</button>
 			</div>
 			{/* <div className='events'>
 				<button className='all-day-event blue event'>
@@ -107,6 +120,113 @@ function CalendarDay({ day, showWeekName, selectedMonth }: CalendarDayProps) {
 					<div className='event-name'>Event Name</div>
 				</button>
 			</div> */}
+			<EventFormModal
+				date={day}
+				isOpen={isNewEventModalOpen}
+				onClose={() => setIsNewEventModalOpen(false)}
+				onSubmit={addEvent}
+			/>
 		</div>
+	)
+}
+
+type EventFormModalProps = {
+	onSubmit: (event: UnionOmit<Event, 'id'>) => void
+} & (
+	| {
+			onDelete: () => void
+			event: Event
+			date?: never
+	  }
+	| { onDelete?: never; event?: never; date: Date }
+) &
+	Omit<ModalProps, 'children'>
+
+function EventFormModal({
+	onSubmit,
+	onDelete,
+	event,
+	date,
+	...modalProps
+}: EventFormModalProps) {
+	return (
+		<Modal {...modalProps}>
+			<div className='overlay'></div>
+			<div className='modal-body'>
+				<div className='modal-title'>
+					<div>Add Event</div>
+					<small>6/8/23</small>
+					<button className='close-btn'>&times;</button>
+				</div>
+				<form>
+					<div className='form-group'>
+						<label htmlFor='name'>Name</label>
+						<input type='text' name='name' id='name' />
+					</div>
+					<div className='form-group checkbox'>
+						<input type='checkbox' name='all-day' id='all-day' />
+						<label htmlFor='all-day'>All Day?</label>
+					</div>
+					<div className='row'>
+						<div className='form-group'>
+							<label htmlFor='start-time'>Start Time</label>
+							<input
+								type='time'
+								name='start-time'
+								id='start-time'
+							/>
+						</div>
+						<div className='form-group'>
+							<label htmlFor='end-time'>End Time</label>
+							<input type='time' name='end-time' id='end-time' />
+						</div>
+					</div>
+					<div className='form-group'>
+						<label>Color</label>
+						<div className='row left'>
+							<input
+								type='radio'
+								name='color'
+								value='blue'
+								id='blue'
+								checked
+								className='color-radio'
+							/>
+							<label htmlFor='blue'>
+								<span className='sr-only'>Blue</span>
+							</label>
+							<input
+								type='radio'
+								name='color'
+								value='red'
+								id='red'
+								className='color-radio'
+							/>
+							<label htmlFor='red'>
+								<span className='sr-only'>Red</span>
+							</label>
+							<input
+								type='radio'
+								name='color'
+								value='green'
+								id='green'
+								className='color-radio'
+							/>
+							<label htmlFor='green'>
+								<span className='sr-only'>Green</span>
+							</label>
+						</div>
+					</div>
+					<div className='row'>
+						<button className='btn btn-success' type='submit'>
+							Add
+						</button>
+						<button className='btn btn-delete' type='button'>
+							Delete
+						</button>
+					</div>
+				</form>
+			</div>
+		</Modal>
 	)
 }
